dca367fd41c47373faccf1f9e96355374ef9a923
XDcobra <xdcobra11@gmail.com>
2026-03-04 12:34:03 +0700

    feat(pcm-live-stream): updated example app to use own streaming api

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,9 +1,40 @@
+import { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { StyleSheet, View } from 'react-native';
+import { Platform, PermissionsAndroid, StyleSheet, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import RootNavigator from './navigation/RootNavigator';
 
+async function requestAndroidPermissions() {
+  if (Platform.OS !== 'android') return;
+  try {
+    const grants = await PermissionsAndroid.requestMultiple([
+      PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+      PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+      PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+    ]);
+
+    if (
+      grants['android.permission.WRITE_EXTERNAL_STORAGE'] ===
+        PermissionsAndroid.RESULTS.GRANTED &&
+      grants['android.permission.READ_EXTERNAL_STORAGE'] ===
+        PermissionsAndroid.RESULTS.GRANTED &&
+      grants['android.permission.RECORD_AUDIO'] ===
+        PermissionsAndroid.RESULTS.GRANTED
+    ) {
+      console.log('All permissions granted');
+    } else {
+      console.log('All required permissions not granted');
+    }
+  } catch (err) {
+    console.warn(err);
+  }
+}
+
 export default function App() {
+  useEffect(() => {
+    requestAndroidPermissions();
+  }, []);
+
   return (
     <SafeAreaProvider>
       <View style={styles.container}>
